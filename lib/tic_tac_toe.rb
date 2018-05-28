e2d60cc7f4e2b37e2121bff79205c2f198932a8b
Scott Hall <scotthall127@gmail.com>
2018-05-28 16:53:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,7 @@ class TicTacToe
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-  def move(board, index, current_player)
-    board[index] = current_player
+  def move(index, current_player)
+    @board[index] = current_player
   end
 end
