1a6d96b71f803be5973672ba88863c461cda2147
Scott Hall <scotthall127@gmail.com>
2018-05-28 16:44:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ class TicTacToe
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
   def display_board
-  puts " #{@board[1]} | #{@board[1]} | #{@board[2]} "
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
