f2a7b71d97e3457f5a785f809b6ae895931b332c
Scott Hall <scotthall127@gmail.com>
2018-05-28 16:54:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,4 +27,7 @@ class TicTacToe
   def move(index, current_player)
     @board[index] = current_player
   end
+  def position_taken?(location)
+    @board[location] != " " && board[location] != ""
+  end
 end
