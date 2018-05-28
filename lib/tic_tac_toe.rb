e5eb447a167ccc71b62033c0374b5bd76f6bf34c
Scott Hall <scotthall127@gmail.com>
2018-05-28 16:51:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
  end
- def input_to_index
-   gets.to_i - 1
- end
+ def input_to_index(user_input)
+  user_input.to_i - 1
+  end
 end
